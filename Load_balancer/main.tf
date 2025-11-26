58c949182753f728f067d38309d5266201ffbb68
kkp-cd <kiran25cd@gmail.com>
2025-11-26 19:57:42 +0530

    Create main.tf

---
+++ Load_balancer/main.tf
@@ -0,0 +1,64 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-tf-lb"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tf-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "tf-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "tf-lb-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_lb" "lb" {
+  name                = "tf-lb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "PublicFrontend"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "backend_pool" {
+  name                = "tf-backend-pool"
+  loadbalancer_id     = azurerm_lb.lb.id
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_lb_probe" "http_probe" {
+  name                = "http-probe"
+  resource_group_name = azurerm_resource_group.rg.name
+  loadbalancer_id     = azurerm_lb.lb.id
+  protocol            = "Tcp"
+  port                = 80
+}
+
+resource "azurerm_lb_rule" "http_lb_rule" {
+  name                           = "http-rule"
+  resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.lb.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "PublicFrontend"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.backend_pool.id]
+  probe_id                       = azurerm_lb_probe.http_probe.id
+}
